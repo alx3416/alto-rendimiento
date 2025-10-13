2762c16b67bf6aeb11f5511bf85015a0c44c030c
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2025-10-13 16:07:43 -0600

    adding pass by reference in opencv gamma correction

--- opencv/image_utils.h
+++ opencv/image_utils.h
@@ -9,26 +9,47 @@
 #include <cmath>
 #include <omp.h>
 
-namespace myLib{
-
-    cv::Mat gammaCorrectionPassByValue(cv::Mat frame_gray){
+namespace myLib
+{
+    cv::Mat gammaCorrectionPassByValue(cv::Mat frame_gray, const float gamma)
+    {
+        cv::Mat result = frame_gray.clone();
         double value = 0;
-        for(int row=0; row<frame_gray.rows; row++){
-            for(int col=0; col<frame_gray.cols; col++){
-                value = static_cast<double>(frame_gray.at<uint8_t >(row, col));
+        for (int row = 0; row < frame_gray.rows; row++)
+        {
+            for (int col = 0; col < frame_gray.cols; col++)
+            {
+                value = static_cast<double>(frame_gray.at<uint8_t>(row, col));
 
-                // TODO Your code here
+                value = value / 255.0;
+                value = std::pow(value, gamma);
+                value = value * 255.0;
 
-                frame_gray.at<uint8_t >(row, col) = static_cast<uint8_t>(value);
+                result.at<uint8_t>(row, col) = static_cast<uint8_t>(value);
             }
         }
-        return frame_gray;
+        return result;
     }
 
-// TODO: code the same gamma correction function but now with pass by reference
+    void gammaCorrectionPassByReference(cv::Mat& source_gray, cv::Mat& result_gray, const float gamma)
+    {
+        double value = 0;
+        for (int row = 0; row < source_gray.rows; row++)
+        {
+            for (int col = 0; col < source_gray.cols; col++)
+            {
+                value = static_cast<double>(source_gray.at<uint8_t>(row, col));
 
-// TODO: code two functions, one for kernel and local patch multiplication, and another one to perform sliding window
+                value = value / 255.0;
+                value = std::pow(value, gamma);
+                value = value * 255.0;
 
+                result_gray.at<uint8_t>(row, col) = static_cast<uint8_t>(value);
+            }
+        }
+    }
+
+    // TODO: code two functions, one for kernel and local patch multiplication, and another one to perform sliding window
 } // namespace myLib
 
 #endif //CPPBASICS_IMAGE_UTILS_H
--- opencv/show_image.cpp
+++ opencv/show_image.cpp
@@ -3,14 +3,20 @@
 
 int main(int, char**)
 {
-    cv::Mat frame, frame_gray;
+    cv::Mat frame, frame_gray, frame_gray_gamma;
     cv::Mat image = cv::imread("baboon.png");
-    if (image.empty()) {
+    if (image.empty())
+    {
         std::cerr << "Failed to load image!" << std::endl;
         return -1;
     }
 
-    cv::imshow("Image", image);
+    cv::cvtColor(image, frame_gray, cv::COLOR_BGR2GRAY);
+    frame_gray_gamma = frame_gray.clone();
+    myLib::gammaCorrectionPassByReference(frame_gray, frame_gray_gamma, 0.25);
+
+    cv::imshow("Image_gray", frame_gray);
+    cv::imshow("Image_gamma", frame_gray_gamma);
     cv::waitKey(0); // Wait for a key press
     return 0;
 }
